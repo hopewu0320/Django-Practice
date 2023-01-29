d9b4d5ac6e402bff066ffa8904c1f6dbecafde04
hopewu0320 <gccbs01596@gmail.com>
2023-01-29 16:26:05 +0800

    first

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 rooms=[
     {'id':1,'name':'Let s learn Python!'},
@@ -7,12 +8,10 @@ rooms=[
     {'id':3,'name':'Fronted developers'}
 ]
 def home(request):
+    rooms = Room.objects.all()
     context={'rooms':rooms}
     return render(request,'base/home.html',context)
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'base/room.html',context)
